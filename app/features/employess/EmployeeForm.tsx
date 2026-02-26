1c40d08237e7e0fb899b76d8d239bc7ca0682ef5
DhruviPattiwala <dhruvipattiwala@gmail.com>
2026-02-26 16:31:52 +0530

    2nd commit

--- app/features/employess/EmployeeForm.tsx
+++ app/features/employess/EmployeeForm.tsx
@@ -107,12 +107,7 @@ export const EmployeeForm = ({ openEmpDialog, closeEmpDialog, editEmployeeData,s
   const handleRemoveSkill = (skill: string) => {
     setSkills(prevSkills => prevSkills.filter(s => s !== skill));
   };
-  {/*  <DialogContent className="w-full max-w-lg m-4 max-h-[90vh] overflow-y-auto p-6 rounded-2xl">
 
- <DialogContent className="w-full max-w-lg max-h-[90vh] overflow-y-auto p-6 rounded-2xl">
-   <div className="space-y-6">
-     <form className="grid grid-cols-1 md:grid-cols-2 gap-4"> */}
-// sm:max-w-2xl  max-h-screen 
   return (
     <>
       <Dialog open={openEmpDialog} onOpenChange={closeEmpDialog}>
